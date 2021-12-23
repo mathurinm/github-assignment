03aa244bfebf5b49a106378a6116380e66bc083a
Asmaesoug <soughou1999@gmail.com>
2021-12-23 22:20:47 +0100

    wallis_product function

--- numpy_questions.py
+++ numpy_questions.py
@@ -37,8 +37,6 @@ def max_index(X):
         If the input is not a numpy error or
         if the shape is not 2D.
     """
-    i = 0
-    j = 0
 
     # TODO
     m = X.max()
@@ -68,4 +66,8 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    pi_2 = 1
+    for i in range(1, n_terms+1):
+        pi_2 *= 4*(i**2)/(4*(i**2)-1)
+        pi = pi_2*2
+    return pi
