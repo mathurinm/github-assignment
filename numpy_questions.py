2fdffc6e8c5522ff30699209834de7b235a4d610
mathieu-peh <116145527+mathieu-peh@users.noreply.github.com>
2022-10-28 17:26:08 +0200

    Update numpy_questions.py

--- numpy_questions.py
+++ numpy_questions.py
@@ -71,7 +71,6 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    pi = 1
 
     if n_terms == 0:
         pi = 1
