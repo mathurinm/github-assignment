ed986586b695f298f4a3dc804c03bbedb0d41b3c
Sophie <sophie.marchal@hec.edu>
2022-11-05 18:24:25 +0100

    v6

--- numpy_questions.py
+++ numpy_questions.py
@@ -19,6 +19,7 @@ import numpy as np
 
 def max_index(X):
     """Return the index of the maximum in a numpy array.
+
     Parameters
     ----------
     X : ndarray of shape (n_samples, n_features)
@@ -33,14 +34,15 @@ def max_index(X):
         If the input is not a numpy error or
         if the shape is not 2D.
     """
+    i = 0
+    j = 0
+
     if not isinstance(X, np.ndarray):
         raise ValueError("The input is not a numpy array")
 
     if len(X.shape) != 2:
         raise ValueError("The shape is not 2D")
 
-    i = 0
-    j = 0
     row_count = 0
     max_val = 0
     for row in X:
@@ -56,6 +58,7 @@ def max_index(X):
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
+    
     See:
     https://en.wikipedia.org/wiki/Wallis_product
     Parameters
@@ -68,8 +71,10 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
+
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
+
     prod = 1
 
     for i in range(1, n_terms+1):
