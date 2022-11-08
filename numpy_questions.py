1f99844c631405b86aebee3a7be858d1c6108656
KenzoBou <kenzo.bounegta@hec.edu>
2022-11-08 11:40:20 +0100

    Commit

--- numpy_questions.py
+++ numpy_questions.py
@@ -37,12 +37,9 @@ def max_index(X):
         If the input is not a numpy error or
         if the shape is not 2D.
     """
-    i = 0
-    j = 0
+    
 
-    # TODO
-
-    return i, j
+    return np.unravel_index(np.argmax(X, axis = None), X.shape)
 
 
 def wallis_product(n_terms):
@@ -64,4 +61,10 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+
+    prod = 1
+    for k in range(1, int(n_terms) + 1):
+        prod *= (4 * k ** 2 ) / ((4 * k ** 2) - 1)
+    return 2 * prod
+
+
