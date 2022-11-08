7247542b3f473dd083559e614d5522bd43c0107c
ConstanceGlly <constanceguelluy@gmail.com>
2022-11-08 20:43:44 +0100

    FINAL

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -1,4 +1,23 @@
 """Assignment - making a sklearn estimator.
+
+The goal of this assignment is to implement by yourself a scikit-learn
+estimator for the OneNearestNeighbor and check that it is working properly.
+
+The nearest neighbor classifier predicts for a point X_i the target y_k of
+the training sample X_k which is the closest to X_i. We measure proximity with
+the Euclidean distance. The model will be evaluated with the accuracy (average
+number of samples corectly classified). You need to implement the `fit`,
+`predict` and `score` methods for this class. The code you write should pass
+the test we implemented. You can run the tests by calling at the root of the
+repo `pytest test_sklearn_questions.py`.
+
+We also ask to respect the pep8 convention: https://pep8.org. This will be
+enforced with `flake8`. You can check that there is no flake8 errors by
+calling `flake8` at the root of the repo.
+
+Finally, you need to write docstring similar to the one in `numpy_questions`
+for the methods you code and for the class. The docstring will be checked using
+`pydocstyle` that you can also call at the root of the repo.
 """
 import numpy as np
 from sklearn.base import BaseEstimator
