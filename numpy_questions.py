29470397d89102ef80c571a0ff8c8793e45e81da
ConstanceGlly <constanceguelluy@gmail.com>
2022-11-08 16:38:50 +0100

    update 5

--- numpy_questions.py
+++ numpy_questions.py
@@ -30,6 +30,7 @@ def max_index(X):
         pass
     # The row and column index of the maximum
     else:
+        global i, j
         i, j = np.unravel_index(X.argmax(), X.shape)
     return i, j
 
@@ -47,7 +48,12 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    wallis = 1
-    for i in range(n_terms+1):
-        wallis = wallis * (4 * i**2 / (4 * i**2 - 1))
-    return wallis * 2
+    if(n_terms == 0):
+        pi = 1
+    
+    else:
+        wallis = 2
+        for k in range(n_terms+1):
+            wallis = wallis * (4 * k**2 / (4 * k**2 - 1))
+        pi = wallis * 2
+    return pi
