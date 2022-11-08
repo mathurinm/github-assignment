932d4037cbdf6950a921929d3612de68f7f771e7
Zofia Smoleń <zofiasmolen@Zofias-MacBook-Air.local>
2022-11-08 14:28:28 +0100

    Commit numpy solutions

--- numpy_questions.py
+++ numpy_questions.py
@@ -15,7 +15,9 @@ We also ask to respect the pep8 convention: https://pep8.org.
 This will be enforced with `flake8`. You can check that there is no flake8
 errors by calling `flake8` at the root of the repo.
 """
+from typing import Tuple
 import numpy as np
+
 
 
 def max_index(X):
@@ -37,10 +39,19 @@ def max_index(X):
         If the input is not a numpy error or
         if the shape is not 2D.
     """
+
     i = 0
     j = 0
+    if (type(X) is not np.ndarray):
+         raise ValueError('Input is not a numpy array.')
+    elif len(X.shape) != 2:
+         raise ValueError('Input is not 2D.')
 
-    # TODO
+    for a in range(len(X)):
+         for b in range(len(X[0])):
+             if X[a][b] > X[i][j]:
+                 i = a
+                 j = b
 
     return i, j
 
@@ -62,6 +73,10 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    
+    pi_approximation = 2.0
+    if n_terms == 0:
+        return pi_approximation
+    for j in range(1, n_terms + 1):
+        pi_approximation *= (4 * j ** 2) / (4 * j ** 2 - 1)
+    return pi_approximation
