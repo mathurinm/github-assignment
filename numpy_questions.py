161630898613f50b119f0d6b5f763ef4772db1a1
octavedbc <octave.dbc@gmail.com>
2022-11-08 18:01:39 +0100

    Numpy Changes

--- numpy_questions.py
+++ numpy_questions.py
@@ -17,51 +17,21 @@ errors by calling `flake8` at the root of the repo.
 """
 import numpy as np
 
+X= np.arange(0, 10).reshape(-1, 1)
 
 def max_index(X):
-    """Return the index of the maximum in a numpy array.
 
-    Parameters
-    ----------
-    X : ndarray of shape (n_samples, n_features)
-        The input array.
-
-    Returns
-    -------
-    (i, j) : tuple(int)
-        The row and columnd index of the maximum.
-
-    Raises
-    ------
-    ValueError
-        If the input is not a numpy error or
-        if the shape is not 2D.
-    """
-    i = 0
-    j = 0
-
-    # TODO
+    i = np.argmax(np.max(X, axis=0))
+    j = np.argmax(np.max(X, axis=1))
 
     return i, j
 
-
 def wallis_product(n_terms):
-    """Implement the Wallis product to compute an approximation of pi.
+    pi = 1.0
 
-    See:
-    https://en.wikipedia.org/wiki/Wallis_product
+    for j in range(1, n_terms):
+        pi *= 4 * j ** 2 / (4 * j ** 2 - 1)
 
-    Parameters
-    ----------
-    n_terms : int
-        Number of steps in the Wallis product. Note that `n_terms=0` will
-        consider the product to be `1`.
-
-    Returns
-    -------
-    pi : float
-        The approximation of order `n_terms` of pi using the Wallis product.
-    """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    pi *= 2
+    
+    return pi
