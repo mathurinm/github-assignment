3899865aaeaa3cef0c42fc2e77f358f69d7b7bee
Bicio7 <116149527+Bicio7@users.noreply.github.com>
2022-11-09 19:09:06 +0100

    Update numpy_questions.py

--- numpy_questions.py
+++ numpy_questions.py
@@ -60,11 +60,11 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    
+ 
     pi = 1
     if n_terms == 0:
         pi = 1
-         
+
     else:
         for i in range(1, n_terms + 1):
             pi = pi * (4 * (i**2)) / ((4 * (i**2)) - 1)
