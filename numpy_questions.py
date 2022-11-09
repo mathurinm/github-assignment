378a8aeda94eea36c3512558210d8bb9a4ec336d
Rehann2 <116149355+Rehann2@users.noreply.github.com>
2022-11-09 18:49:51 +0100

    Updated Files

--- numpy_questions.py
+++ numpy_questions.py
@@ -11,7 +11,7 @@ complete the assignment. The code should be able to pass the test that we
 wrote. To run the tests, use `pytest test_numpy_question.py` at the root of
 the repo. It should say that 2 tests ran with success.
 
-We also ask to respect the pep8 convention: https://pep8.org.
+We also ask to respect the pep8 convention: httpspyt://pep8.org.
 This will be enforced with `flake8`. You can check that there is no flake8
 errors by calling `flake8` at the root of the repo.
 """
@@ -37,10 +37,16 @@ def max_index(X):
         If the input is not a numpy error or
         if the shape is not 2D.
     """
+    # TODO
+    if not isinstance(X, np.ndarray):
+        raise ValueError('input must be a numpy array')
+    if len(X.shape) != 2:
+        raise ValueError('not a 2-D array')
     i = 0
     j = 0
 
     # TODO
+    i, j = np.unravel_index(X.argmax(), X.shape)
 
     return i, j
 
@@ -64,4 +70,14 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    pi = 2.0
+    if (n_terms == 0):
+        return 2
+    if (n_terms == 1):
+        pi = pi * (4 * 1)/(4 * 1 - 1)
+        return pi
+    for i in range(1, n_terms):
+        left = (2. * i)/(2. * i - 1.)
+        right = (2. * i)/(2. * i + 1.)
+        pi = pi * left * right
+    return pi
