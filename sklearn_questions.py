4751b578fe64517d551e04c73744b5758a36518c
Bicio7 <116149527+Bicio7@users.noreply.github.com>
2022-11-09 19:57:52 +0100

    Update sklearn_questions.py

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -35,6 +35,7 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
 
     def fit(self, X, y):
         """Parameters.
+
         X is an array corresponding to the training data
         y is an array corresponding to the target data
 
