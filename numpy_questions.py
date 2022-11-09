fd1205410faa3951f7536be5331a0fbdec77660e
Bicio7 <116149527+Bicio7@users.noreply.github.com>
2022-11-09 19:11:43 +0100

    Update numpy_questions.py

--- numpy_questions.py
+++ numpy_questions.py
@@ -38,7 +38,7 @@ def max_index(X):
         raise ValueError('the input is not a numpy array')
 
     if X.ndim != 2:
-        raise ValueError('the input is not 2-dimensional')    
+        raise ValueError('the input is not 2-dimensional')
     i, j = np.unravel_index(np.argmax(X, axis=None), X.shape)
 
     return i, j
@@ -60,7 +60,7 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
- 
+
     pi = 1
     if n_terms == 0:
         pi = 1
