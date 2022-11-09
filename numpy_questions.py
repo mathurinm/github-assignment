41becbc76ecef9b64f4f2d57e06a6e67c8d9e6ef
alvarocalafell <116169930+alvarocalafell@users.noreply.github.com>
2022-11-09 19:26:50 +0100

    Update numpy_questions.py

--- numpy_questions.py
+++ numpy_questions.py
@@ -12,6 +12,7 @@ We also ask to respect the pep8 convention: https://pep8.org.
 This will be enforced with `flake8`. You can check that there is no flake8
 errors by calling `flake8` at the root of the repo.
 """
+
 import numpy as np
 
 
@@ -31,6 +32,7 @@ def max_index(X):
         If the input is not a numpy error or
         if the shape is not 2D.
     """
+
     i = 0
     j = 0
 
@@ -57,9 +59,9 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
+
     result = 1
     for count in range(2, 2*(n_terms+1), 2):
         result *= ((count*count)/((count-1)*(count+1)))
