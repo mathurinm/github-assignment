a5135d4af43911a39911a7d4a81c5e193b749df7
alvarocalafell <116169930+alvarocalafell@users.noreply.github.com>
2022-11-09 18:57:14 +0100

    Update sklearn_questions.py

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -70,7 +70,6 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         check_is_fitted(self)
         X = check_array(X)
         dtype = self.classes_.dtype
-        )
         y_pred=np.full(
             shape = len(X), fill_value = self.classes_[0],
             dtype = self.classes_.dtype
