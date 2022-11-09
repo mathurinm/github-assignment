0b48351dbe7c40817923f03efe6d56964f383c5d
Bicio7 <116149527+Bicio7@users.noreply.github.com>
2022-11-09 19:07:05 +0100

    Update numpy_questions.py

--- numpy_questions.py
+++ numpy_questions.py
@@ -14,6 +14,7 @@ errors by calling `flake8` at the root of the repo.
 """
 import numpy as np
 
+
 def max_index(X):
     """Return the index of the maximum in a numpy array.
     Parameters
@@ -41,6 +42,7 @@ def max_index(X):
     i, j = np.unravel_index(np.argmax(X, axis=None), X.shape)
 
     return i, j
+
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
