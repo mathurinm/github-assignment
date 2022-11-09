d650ebf73801235d89e5e5c8ce88a217a840325d
alvarocalafell <116169930+alvarocalafell@users.noreply.github.com>
2022-11-09 19:33:53 +0100

    Update numpy_questions.py

--- numpy_questions.py
+++ numpy_questions.py
@@ -3,13 +3,11 @@ The goals of this assignment are:
     * Use numpy in practice with two easy exercises.
     * Use automated tools to validate the code (`pytest` and `flake8`)
     * Submit a Pull-Request on github to practice `git`.
-    
 The two functions below are skeleton functions. The docstrings explain what
 are the inputs, the outputs and the expected error. Fill the function to
 complete the assignment. The code should be able to pass the test that we
 wrote. To run the tests, use `pytest test_numpy_question.py` at the root of
 the repo. It should say that 2 tests ran with success.
-
 We also ask to respect the pep8 convention: https://pep8.org.
 This will be enforced with `flake8`. You can check that there is no flake8
 errors by calling `flake8` at the root of the repo.
@@ -43,9 +41,8 @@ def max_index(X):
         raise ValueError("Shape of the input is not 2-dimensional")
          
     i, j = np.unravel_index(np.argmax(X, axis=None), X.shape)
-   
-    return i, j
 
+    return i, j
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
