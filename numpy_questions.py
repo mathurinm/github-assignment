d603bfcdb144bad0afce91c15d0dbfcdb8ed574c
Trung-Dan-Phan <trung-dan.phan@polytechnique.edu>
2023-11-20 17:01:12 +0100

    Git Part 2

--- numpy_questions.py
+++ numpy_questions.py
@@ -37,9 +37,6 @@ def max_index(X):
         If the input is not a numpy error or
         if the shape is not 2D.
     """
-    i = 0
-    j = 0
-
     if not isinstance(X, np.ndarray) or X.ndim != 2:
         raise ValueError("Input must be a 2D numpy array.")
 
@@ -64,14 +61,13 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    #XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
+    #The n_terms is an int that corresponds to the number of terms in the product. For example 10000.
 
     if n_terms == 0:
         return 2.0
 
     result = 1
     for i in range(1, n_terms + 1):
-        result *= (4 * i**2) / ((4 * i**2) - 1)
-        
+        result *= (4 * i**2) / ((4 * i**2) - 1)   
     return 2 * result
+    
