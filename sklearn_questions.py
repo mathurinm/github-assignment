1ed0232a7ee9a919582853bec2ef2e73b59e3b03
S Mesguiche <smesguiche@MacBook-Pro-de-S.local>
2023-11-20 17:24:27 +0100

    Add modifications to sklearn and numpy

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -31,7 +31,7 @@ from sklearn.utils.multiclass import check_classification_targets
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
     """OneNearestNeighbor classifier."""
 
-    def __init__(self): # noqa: D107
+    def __init__(self):  # noqa: D107
         pass
 
     def fit(self, X, y):
