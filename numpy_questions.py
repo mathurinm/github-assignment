cf17aa6d3fdd22353a587cef32dc9534ad0da33f
elisebaratt <71435423+elisebaratt@users.noreply.github.com>
2023-11-20 18:30:15 +0100

    Elise Barattini Part B

--- numpy_questions.py
+++ numpy_questions.py
@@ -17,53 +17,23 @@ errors by calling `flake8` at the root of the repo.
 """
 import numpy as np
 
-
 def max_index(X):
-    """Return the index of the maximum in a numpy array.
-
-    Parameters
-    ----------
-    X : ndarray of shape (n_samples, n_features)
-        The input array.
-
-    Returns
-    -------
-    (i, j) : tuple(int)
-        The row and column index of the maximum.
-
-    Raises
-    ------
-    ValueError
-        If the input is not a numpy array or
-        if the shape is not 2D.
-    """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-        raise ValueError("Input must be a 2D numpy array")
+    """Return the index of the maximum in a numpy array."""
+    if not X.ndim != 2 or isinstance(X, np.ndarray):
+        raise ValueError("input must be a two-dimensional numpy array.")
 
     i, j = np.unravel_index(np.argmax(X), X.shape)
     return i, j
 
 
 def wallis_product(n_terms):
-    """Implement the Wallis product to compute an approximation of pi.
-
-    Parameters
-    ----------
-    n_terms : int
-        Number of steps in the Wallis product. Note that `n_terms=0` will
-        consider the product to be `1`.
-
-    Returns
-    -------
-    pi : float
-        The approximation of order `n_terms` of pi using the Wallis product.
-    """
+    """Implement the Wallis product to compute an approximation of pi."""
     if n_terms == 0:
         return 2.0
 
-    product = 1.0
-    for i in range(1, n_terms + 1):
-        term = (2.0 * i) / (2.0 * i - 1) * (2.0 * i) / (2.0 * i + 1)
-        product *= term
+    n = np.arange(1, n_terms + 1)
+    terms = (2.0 * n / (2.0 * n - 1)) * (2.0 * n / (2.0 * n + 1))
+    pi_approximation = np.prod(terms)
 
-    return product * 2
+    return pi_approximation * 2
+
