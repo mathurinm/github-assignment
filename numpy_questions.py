98de788c0f4554397795d171f2643e2ef857a058
Julien Pezzi <julien.pezzi@outlook.fr>
2023-11-21 11:52:35 +0100

    Completed and passed the test for the Numpy assignment

--- numpy_questions.py
+++ numpy_questions.py
@@ -37,19 +37,16 @@ def max_index(X):
         If the input is not a numpy error or
         if the shape is not 2D.
     """
-    i = 0
-    j = 0
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        raise ValueError("Input must be a 2D numpy array.")
 
-    # TODO
+    max_index = np.unravel_index(np.argmax(X), X.shape)
 
-    return i, j
+    return max_index
 
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
-
-    See:
-    https://en.wikipedia.org/wiki/Wallis_product
 
     Parameters
     ----------
@@ -62,6 +59,12 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    if n_terms == 0:
+        return 2.0
+
+    result = 1.0
+    for i in range(1, n_terms + 1):
+        term = (4.0 * i**2) / ((4.0 * i**2) - 1)
+        result *= term
+
+    return 2 * result
