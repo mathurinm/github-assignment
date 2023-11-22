303effc7c5c3dfcbbd99f652768317033c51ee69
Jean-Eudes <eudes.agbre@gmail.com>
2023-11-22 14:32:26 +0100

    Correction Wallis Prod

--- numpy_questions.py
+++ numpy_questions.py
@@ -68,6 +68,8 @@ def wallis_product(n_terms):
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
 
+    if n_terms == 0:
+        return 1.0
     terms = np.arange(1, n_terms+1)
     terms = 2*terms/(2*terms - 1)*2*terms/(2*terms + 1)
 
