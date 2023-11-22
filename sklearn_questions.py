6c1de4f31cf6b4cbe955d470cb64539f61f0e0f5
Artus Mathieu <artus.mathieu5@gmail.com>
2023-11-22 21:07:40 +0100

    Modified sklearn

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -26,30 +26,50 @@ from sklearn.utils.validation import check_X_y
 from sklearn.utils.validation import check_array
 from sklearn.utils.validation import check_is_fitted
 from sklearn.utils.multiclass import check_classification_targets
+from sklearn.metrics import pairwise_distances
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    "OneNearestNeighbor classifier."
+    """OneNearestNeighbor classifier."""
 
     def __init__(self):  # noqa: D107
         pass
 
     def fit(self, X, y):
-        """Write docstring.
+        """Train the model.
 
-        And describe parameters
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            Training data.
+        y : array-like, shape (n_samples,)
+            Target values.
         """
         X, y = check_X_y(X, y)
         check_classification_targets(y)
         self.classes_ = np.unique(y)
 
-        # XXX fix
+        self.X_train_ = X.copy()
+        self.y_train_ = y.copy()
+        self.n_features_in_ = X.shape[1]
+
         return self
 
     def predict(self, X):
-        """Write docstring.
+        """
+        Given a matrix of distances between test points and training points.
 
-        And describe parameters
+        Predict a label for each test point.
+
+        Inputs:
+        - X: A numpy array of shape (num_test, num_train) where X[i, j]
+        gives the distance betwen the ith test
+        point and the jth training point.
+
+        Returns:
+        - y: A numpy array of shape (num_test,)
+        containing predicted labels for the
+        test data, where y[i] is the predicted label for the test point X[i].
         """
         check_is_fitted(self)
         X = check_array(X)
@@ -59,15 +79,30 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         )
 
         # XXX fix
+
+        dists = pairwise_distances(X, self.X_train_)
+        nearest = np.argsort(dists)[:, 0]
+        for i in range(len(X)):
+            y_pred[i] = self.y_train_[nearest[i]]
+
         return y_pred
 
     def score(self, X, y):
-        """Write docstring.
+        """
+        Return the mean accuracy on the given test data and labels.
 
-        And describe parameters
+        Parameters.
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Test samples.
+        y : array-like of shape (n_samples,)
+            True labels for X.
+        Returns
+        -------
+        accuracy : float
+            Mean accuracy of self.predict(X) w.r.t. y.
         """
         X, y = check_X_y(X, y)
-        y_pred = self.predict(X)
-
         # XXX fix
-        return y_pred.sum()
+        accuracy = np.mean(self.predict(X) == y)
+        return accuracy
