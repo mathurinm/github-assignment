f5b8b2d73b0b08f3b1ef5b8e866ffb74ee9e9598
Boyenval Thibaut <boyenval.thibaut@gmail.com>
2023-11-24 13:56:32 +0100

    Numpy exercices updated 2

--- numpy_questions.py
+++ numpy_questions.py
@@ -68,9 +68,9 @@ def wallis_product(n_terms):
         The approximation of order `n_terms` of pi using the Wallis product.
     """
     if n_terms == 0:
-        return 1.0
+        return 2.0
 
-    pi_approx = 1.0
+    pi_approx = 2.0
     for i in range(1, n_terms + 1):
         numerator = 4 * i**2
         denominator = 4 * i**2 - 1
