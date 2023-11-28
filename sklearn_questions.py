49483798711d28b80a159fa60b1582f8d280e015
Hocine ZIDI <zidih23@gmail.com>
2023-11-28 01:07:52 +0100

    Debbugging part B sklearn, 4th trial

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -1,70 +1,31 @@
 import numpy as np
-from sklearn.base import BaseEstimator
-from sklearn.base import ClassifierMixin
-from sklearn.utils.validation import check_X_y
-from sklearn.utils.validation import check_array
-from sklearn.utils.validation import check_is_fitted
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.multiclass import check_classification_targets
-
+from sklearn.utils.multiclass import unique_labels
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
     """
     OneNearestNeighbor classifier.
-
     A simple implementation of the nearest neighbor classifier which predicts
     the target of a new point as the target of the closest point in the training set.
     Proximity is measured using the Euclidean distance.
-
-    Methods
-    -------
-    fit(X, y)
-        Fit the model using X as training data and y as target values.
-    predict(X)
-        Predict the class labels for the provided data.
-    score(X, y)
-        Returns the mean accuracy on the given test data and labels.
     """
-
-    def __init__(self):
-        # Initialize without setting attributes
-        pass
 
     def fit(self, X, y):
         """
         Fit the OneNearestNeighbor model to the training data.
-
-        Parameters
-        ----------
-        X : ndarray of shape (n_samples, n_features)
-            Training data.
-        y : ndarray of shape (n_samples,)
-            Target values.
-
-        Returns
-        -------
-        self : object
-            Returns the instance itself.
         """
         X, y = check_X_y(X, y)
         check_classification_targets(y)
-        # Set attributes during fit
         self.X_ = X
         self.y_ = y
+        self.classes_ = unique_labels(y)  # Store unique classes seen during fit
         return self
 
     def predict(self, X):
         """
         Predict the class labels for the provided data.
-
-        Parameters
-        ----------
-        X : ndarray of shape (n_samples, n_features)
-            Test data.
-
-        Returns
-        -------
-        y_pred : ndarray of shape (n_samples,)
-            Predicted class labels for each sample in X.
         """
         check_is_fitted(self)
         X = check_array(X)
@@ -80,18 +41,6 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
     def score(self, X, y):
         """
         Return the mean accuracy on the given test data and labels.
-
-        Parameters
-        ----------
-        X : ndarray of shape (n_samples, n_features)
-            Test data.
-        y : ndarray of shape (n_samples,)
-            True labels for X.
-
-        Returns
-        -------
-        score : float
-            Mean accuracy of the predictions.
         """
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
