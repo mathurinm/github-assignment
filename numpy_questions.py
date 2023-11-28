ad310a44e39f5638f37932249ef4f714dc77a042
Benjamin Cerf <benjamincerfpro@gmail.com>
2023-11-28 11:12:59 +0100

    modified the code to get rid of flake8 and pydocstyle errors

--- numpy_questions.py
+++ numpy_questions.py
@@ -19,16 +19,34 @@ import numpy as np
 
 
 def max_index(X):
-  
-    if not isinstance(X, np.ndarray): # check if it is a Numpy array 
+    """Return the index of the maximum in a numpy array.
+
+    Parameters
+    ----------
+    X : ndarray of shape (n_samples, n_features)
+        The input array.
+
+    Returns
+    -------
+    (i, j) : tuple(int)
+        The row and columnd index of the maximum.
+
+    Raises
+    ------
+    ValueError
+        If the input is not a numpy array or
+        if the shape is not 2D.
+    """
+    if not isinstance(X, np.ndarray):  # check if it is a Numpy array
         raise ValueError("The input is not a Numpy array")
-    
-    if X.ndim != 2: # check if the array is 2D
+
+    if X.ndim != 2:  # check if the array is 2D
         raise ValueError("Input array must be 2D.")
 
-    max_index = np.unravel_index(np.argmax(X), X.shape) # finds the index of maximum value
-    
-    return max_index
+    # finds the index of maximum value
+    i, j = np.unravel_index(np.argmax(X), X.shape)
+
+    return i, j
 
 
 def wallis_product(n_terms):
@@ -36,13 +54,14 @@ def wallis_product(n_terms):
 
     See:
     https://en.wikipedia.org/wiki/Wallis_product
-"""
+    """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-   
-    p = 1 # Initilization
 
-    for n in range(1, n_terms+1): 
-        p *= 4 * n ** 2 / (4 * n ** 2 - 1) # Implement the Wallis product, here p = pi/2
+    p = 1  # Initilization
 
-    return 2*p # Have to multiply p by 2 to get pi
+    for n in range(1, n_terms+1):
+        # Implement the Wallis product, here p = pi/2
+        p *= 4 * n ** 2 / (4 * n ** 2 - 1)
+
+    return 2*p  # Have to multiply p by 2 to get pi
