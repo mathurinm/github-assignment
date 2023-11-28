582dbda3dd00b7669d014e3cd22dca1c61574f9f
Hocine ZIDI <zidih23@gmail.com>
2023-11-28 02:09:24 +0100

    PS I couldnt get the test to work locally hence this many commits

--- get-pip.py
+++ get-pip.py
@@ -9,8 +9,8 @@
 #
 # Pip is a thing that installs packages, pip itself is a package that someone
 # might want to install, especially if they're looking to run this get-pip.py
-# script. Pip has a lot of code to deal 
-#with the security of installing
+# script. Pip has a lot of code to deal
+# with the security of installing
 # packages, various edge cases on various platforms, and other such sort of
 # "tribal knowledge" that has been encoded in its code base. Because of this
 # we basically include an entire copy of pip inside this blob. We do this
@@ -36,7 +36,8 @@ if this_python < min_version:
     message_parts = [
         "This script does not work on Python {}.{}".format(*this_python),
         "The minimum supported Python version is {}.{}.".format(*min_version),
-        "Please use https://bootstrap.pypa.io/pip/{}.{}/get-pip.py instead.".format(*this_python),
+        "Please use https://bootstrap.pypa.io/pip/{}.{}/get-pip.py"
+        "instead.".format(*this_python),
     ]
     print("ERROR: " + " ".join(message_parts))
     sys.exit(1)
