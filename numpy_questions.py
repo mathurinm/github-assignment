b833bcc7620538590817a352926571bdd8c7eba1
zakatlassi <zakaria.atlassi-douch@hec.edu>
2023-12-10 00:10:02 +0100

    Fixed PEP8 style issues and docstrings, unit tests passed

--- numpy_questions.py
+++ numpy_questions.py
@@ -29,7 +29,7 @@ def max_index(X):
     Returns
     -------
     (i, j) : tuple(int)
-        The row and columnd index of the maximum.
+        The row and column index of the maximum.
 
     Raises
     ------
@@ -37,11 +37,10 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-    i = 0
-    j = 0
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        raise ValueError("Input must be a 2D numpy array")
 
-    # TODO
-
+    i, j = np.unravel_index(np.argmax(X), X.shape)
     return i, j
 
 
@@ -62,6 +61,8 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    pi = 2.0
+    for i in range(1, n_terms + 1):
+        pi *= (4 * i ** 2) / ((2 * i - 1) * (2 * i + 1))
+
+    return pi
--- sklearn_questions.py
+++ sklearn_questions.py
@@ -25,50 +25,77 @@ from sklearn.base import ClassifierMixin
 from sklearn.utils.validation import check_X_y
 from sklearn.utils.validation import check_array
 from sklearn.utils.validation import check_is_fitted
-from sklearn.utils.multiclass import check_classification_targets
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    "OneNearestNeighbor classifier."
+    """OneNearestNeighbor classifier."""
 
-    def __init__(self):  # noqa: D107
+    def __init__(self):
+        """Initialise OneNearestNeighbor classifier."""
         pass
 
-    def fit(self, X, y):
-        """Write docstring.
 
-        And describe parameters
+    def fit(self, X, y):
+        """Fit the model using X as training data and y as target values.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            Training data.
+        y : array-like, shape (n_samples,)
+            Target values.
+
+        Returns
+        -------
+        self : object
         """
         X, y = check_X_y(X, y)
-        check_classification_targets(y)
-        self.classes_ = np.unique(y)
-        self.n_features_in_ = X.shape[1]
-
-        # XXX fix
+        self.X_ = X
+        self.y_ = y
         return self
 
     def predict(self, X):
-        """Write docstring.
+        """Predict the class labels for the provided data.
 
-        And describe parameters
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            Test samples.
+
+        Returns
+        -------
+        y_pred : array, shape (n_samples,)
+            Class labels for each data sample.
         """
         check_is_fitted(self)
         X = check_array(X)
-        y_pred = np.full(
-            shape=len(X), fill_value=self.classes_[0],
-            dtype=self.classes_.dtype
-        )
 
-        # XXX fix
+        # Implement the logic for finding the nearest neighbor
+        # TODO: Implement nearest neighbor search
+        y_pred = np.empty(X.shape[0], dtype=self.y_.dtype)
+
+        for idx, x in enumerate(X):
+            distances = np.sqrt(np.sum((self.X_ - x) ** 2, axis=1))
+            nearest_neighbor_idx = np.argmin(distances)
+            y_pred[idx] = self.y_[nearest_neighbor_idx]
+
         return y_pred
 
     def score(self, X, y):
-        """Write docstring.
+        """Return the mean accuracy on the given test data and labels.
 
-        And describe parameters
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            Test samples.
+        y : array-like, shape (n_samples,)
+            True labels for X.
+
+        Returns
+        -------
+        score : float
+            Mean accuracy of self.predict(X) wrt. y.
         """
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
-
-        # XXX fix
-        return y_pred.sum()
+        return np.mean(y_pred == y)
