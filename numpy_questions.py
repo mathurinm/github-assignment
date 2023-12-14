423e3f0f1902eadbcbea05d7a65984813332bec9
raphaelamzallag <65189486+raphaelamzallag@users.noreply.github.com>
2023-12-14 15:12:14 +0100

    updated code

--- numpy_questions.py
+++ numpy_questions.py
@@ -25,12 +25,10 @@ def max_index(X):
     ----------
     X : ndarray of shape (n_samples, n_features)
         The input array.
-
     Returns
     -------
     (i, j) : tuple(int)
         The row and columnd index of the maximum.
-
     Raises
     ------
     ValueError
@@ -39,14 +37,10 @@ def max_index(X):
     """
     if not isinstance(X, np.ndarray):
         raise ValueError("Input must be a numpy array.")
-
     if X.ndim != 2:
         raise ValueError("Input array must be 2D.")
-
     max_index = np.argmax(X)
-
     max_index_2d = np.unravel_index(max_index, X.shape)
-
     return max_index_2d[0], max_index_2d[1]
 
 
@@ -55,23 +49,19 @@ def wallis_product(n_terms):
 
     See:
     https://en.wikipedia.org/wiki/Wallis_product
-
     Parameters
     ----------
     n_terms : int
         Number of steps in the Wallis product. Note that `n_terms=0` will
         consider the product to be `1`.
-
     Returns
     -------
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
     if n_terms == 0:
-        return 1.0
-
+        return 2.0
     product = 1.0
     for i in range(1, n_terms + 1):
         product *= (4 * i ** 2) / (4 * i ** 2 - 1)
-
     return product * 2
--- sklearn_questions.py
+++ sklearn_questions.py
@@ -19,6 +19,7 @@ Finally, you need to write docstring similar to the one in `numpy_questions`
 for the methods you code and for the class. The docstring will be checked using
 `pydocstyle` that you can also call at the root of the repo.
 """
+
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.metrics import accuracy_score
@@ -27,7 +28,7 @@ from sklearn.utils.multiclass import check_classification_targets
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    "OneNearestNeighbor classifier."
+    """OneNearestNeighbor classifier."""
 
     def __init__(self):  # noqa: D107
         pass
@@ -41,19 +42,17 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
             Training data.
         y : array-like, shape (n_samples,)
             Target values.
-
         Returns:
         self : object
             Returns self.
         """
         X, y = check_X_y(X, y)
         check_classification_targets(y)
+        self.classes_ = np.unique(y)
         self.X_ = X
         self.y_ = y
         self.n_features_in_ = X.shape[1]
-
         return self
-
 
     def predict(self, X):
         """
@@ -62,7 +61,6 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         Parameters:
         X : array-like, shape (n_samples, n_features)
             Test samples.
-
         Returns:
         y_pred : array, shape (n_samples,)
             Class labels for each data sample.
@@ -74,7 +72,6 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
             distances = np.sqrt(np.sum((self.X_ - x) ** 2, axis=1))
             nearest_index = np.argmin(distances)
             return self.y_[nearest_index]
-
         y_pred = np.array([find_nearest_neighbor(x) for x in X])
         return y_pred
 
@@ -87,13 +84,10 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
             Test samples.
         y : array-like, shape (n_samples,)
             True labels for X.
-
         Returns:
         score : float
             Mean accuracy of self.predict(X) wrt. y.
         """
-
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
-
         return accuracy_score(y, y_pred)
