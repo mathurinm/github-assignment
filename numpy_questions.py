b8ebd265b8d5b143e97b4e6f3c6830bc42a5347c
apollineboillot <apolline.boillot@polytechnique.edu>
2023-12-15 17:07:17 +0100

    Update numpy_questions.py

--- numpy_questions.py
+++ numpy_questions.py
@@ -34,7 +34,7 @@ def max_index(X):
     Raises
     ------
     ValueError
-        If the input is not a numpy array or
+        If the input is not a numpy error or
         if the shape is not 2D.
     """
     i = 0
