463c1d1e7560aa4ea0d05a62904b2a9630edcce5
apollineboillot <apolline.boillot@polytechnique.edu>
2023-12-15 17:13:00 +0100

    Update numpy_questions.py

--- numpy_questions.py
+++ numpy_questions.py
@@ -1,25 +1,9 @@
-"""Assignment - using numpy and making a PR.
-
-The goals of this assignment are:
-    * Use numpy in practice with two easy exercises.
-    * Use automated tools to validate the code (`pytest` and `flake8`)
-    * Submit a Pull-Request on github to practice `git`.
-
-The two functions below are skeleton functions. The docstrings explain what
-are the inputs, the outputs and the expected error. Fill the function to
-complete the assignment. The code should be able to pass the test that we
-wrote. To run the tests, use `pytest test_numpy_question.py` at the root of
-the repo. It should say that 2 tests ran with success.
-
-We also ask to respect the pep8 convention: https://pep8.org.
-This will be enforced with `flake8`. You can check that there is no flake8
-errors by calling `flake8` at the root of the repo.
-"""
 import numpy as np
 
 
 def max_index(X):
-    """Return the index of the maximum in a numpy array.
+    """
+    Return the index of the maximum in a numpy array.
 
     Parameters
     ----------
@@ -29,24 +13,24 @@ def max_index(X):
     Returns
     -------
     (i, j) : tuple(int)
-        The row and columnd index of the maximum.
+        The row and column index of the maximum.
 
     Raises
     ------
     ValueError
-        If the input is not a numpy error or
+        If the input is not a numpy array or
         if the shape is not 2D.
     """
-    i = 0
-    j = 0
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        raise ValueError("Input must be a 2D numpy array")
 
-    # TODO
-
-    return i, j
+    max_value_index = np.unravel_index(np.argmax(X, axis=None), X.shape)
+    return max_value_index
 
 
 def wallis_product(n_terms):
-    """Implement the Wallis product to compute an approximation of pi.
+    """
+    Implement the Wallis product to compute an approximation of pi.
 
     See:
     https://en.wikipedia.org/wiki/Wallis_product
@@ -62,6 +46,11 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    if n_terms == 0:
+        return 2.0
+
+    pi_over_two = 1.0
+    for i in range(1, n_terms + 1):
+        pi_over_two *= (4.0 * i ** 2) / ((4.0 * i ** 2) - 1)
+
+    return pi_over_two * 2
