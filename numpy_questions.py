bc863e68cccba6672b9249f0e1e86d15f005b485
apollineboillot <apolline.boillot@polytechnique.edu>
2023-12-15 17:21:58 +0100

    Update numpy_questions.py

--- numpy_questions.py
+++ numpy_questions.py
@@ -1,56 +1,38 @@
 import numpy as np
-
-
 def max_index(X):
-    """
-    Return the index of the maximum in a numpy array.
-
+    """Return the index of the maximum in a numpy array.
     Parameters
     ----------
     X : ndarray of shape (n_samples, n_features)
         The input array.
-
     Returns
     -------
     (i, j) : tuple(int)
-        The row and column index of the maximum.
-
+        The row and columnd index of the maximum.
     Raises
     ------
     ValueError
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-        raise ValueError("Input must be a 2D numpy array")
 
-    max_value_index = np.unravel_index(np.argmax(X, axis=None), X.shape)
-    return max_value_index
+    if not isinstance(X, np.ndarray):
+        raise ValueError("X should be a numpy array")
 
+    if X.ndim != 2:
+        raise ValueError("X should be a 2D array")
 
-def wallis_product(n_terms):
-    """
-    Implement the Wallis product to compute an approximation of pi.
+    i = 0
+    j = 0
 
-    See:
-    https://en.wikipedia.org/wiki/Wallis_product
+    i, j = np.unravel_index(X.flatten().argmax(), shape=X.shape)
 
-    Parameters
-    ----------
-    n_terms : int
-        Number of steps in the Wallis product. Note that `n_terms=0` will
-        consider the product to be `1`.
+    return i, j
 
-    Returns
-    -------
     pi : float
-        The approximation of order `n_terms` of pi using the Wallis product.
-    """
-    if n_terms == 0:
-        return 2.0
+        The approximation of order ⁠ n_terms ⁠ of pi using the Wallis product.
 
-    pi_over_two = 1.0
-    for i in range(1, n_terms + 1):
-        pi_over_two *= (4.0 * i ** 2) / ((4.0 * i ** 2) - 1)
+    unit_term = np.arange(1, n_terms + 1, 1, dtype=np.float64)
+    unit_term = unit_term ** 2
 
-    return pi_over_two * 2
+    return 2 * np.prod(4*unit_term / (4 * unit_term - 1))
