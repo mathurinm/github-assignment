c785567af44f90679796967a4341ddfe740ece08
nicolamassari <nicola.massari1705@gmail.com>
2023-12-16 13:32:18 +0100

    Edited question files

--- numpy_questions.py
+++ numpy_questions.py
@@ -37,10 +37,10 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-    i = 0
-    j = 0
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        raise ValueError("Input must be a 2D numpy array")
 
-    # TODO
+    i, j = np.unravel_index(np.argmax(X), X.shape)
 
     return i, j
 
@@ -62,6 +62,12 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    if n_terms == 0:
+        return 2.0
+
+    prod = 1.0
+    for i in range(1, n_terms + 1):
+        prod *= (4 * i**2) / (4 * i**2 - 1)
+
+    pi = 2 * prod
+    return pi
--- sklearn_questions.py
+++ sklearn_questions.py
@@ -29,28 +29,51 @@ from sklearn.utils.multiclass import check_classification_targets
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    "OneNearestNeighbor classifier."
+    """OneNearestNeighbor classifier."""
 
     def __init__(self):  # noqa: D107
         pass
 
     def fit(self, X, y):
-        """Write docstring.
+        """Fit the OneNearestNeighbor classifier object.
 
-        And describe parameters
+        Parameters
+        ----------
+        X : array-like or pd.DataFrame, shape (n_samples, n_features)
+            Training data.
+
+        y : array-like or pd.Series, shape (n_samples,)
+            Target values.
+
+        Returns
+        -------
+        self : object
+            Returns self.
+
         """
         X, y = check_X_y(X, y)
         check_classification_targets(y)
         self.classes_ = np.unique(y)
         self.n_features_in_ = X.shape[1]
 
-        # XXX fix
+        self.X_train_ = X
+        self.y_train_ = y
+
         return self
 
     def predict(self, X):
-        """Write docstring.
+        """Predict the target values for the input data.
 
-        And describe parameters
+        Parameters
+        ----------
+        X : array-like or pd.DataFrame, shape (n_samples, n_features)
+            Input data for prediction.
+
+        Returns
+        -------
+        y_pred : ndarray, shape (n_samples,)
+            Predicted target values.
+
         """
         check_is_fitted(self)
         X = check_array(X)
@@ -59,16 +82,34 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
             dtype=self.classes_.dtype
         )
 
-        # XXX fix
+        dist = np.linalg.norm(
+            X[:, np.newaxis, :] - self.X_train_[np.newaxis, :, :], axis=2
+        )
+
+        ix = np.argmin(dist, axis=1)
+
+        y_pred = self.y_train_[ix]
+
         return y_pred
 
     def score(self, X, y):
-        """Write docstring.
+        """Return the accuracy on the given test data and targets.
 
-        And describe parameters
+        Parameters
+        ----------
+        X : array-like or pd.DataFrame, shape (n_samples, n_features)
+            Test samples.
+
+        y : array-like or pd.Series, shape (n_samples,)
+            True labels for X.
+
+        Returns
+        -------
+        score : float
+            Mean accuracy of self.predict(X) with respect to y.
+
         """
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
 
-        # XXX fix
-        return y_pred.sum()
+        return (y_pred == y).mean()
