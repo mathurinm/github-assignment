fcffee52d9b7542d6f56b823554bc2d434a50ded
TLiemNGY <thanh-liem.nguyen.2023@polytechnique.edu>
2023-12-18 17:12:00 +0100

    part B of assignment

--- numpy_questions.py
+++ numpy_questions.py
@@ -65,10 +65,13 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    product = 4/3
-    for i in range(2,10000):
-        product = product * ((4*i**2) / (4*i**2 - 1))
+    if n_terms == 0:
+        product = 1
+    else:
+        product = 4/3
+        for i in range(2,n_terms):
+            product = product * ((4*i**2) / (4*i**2 - 1))
     
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return product
+    return 2*product
