027103640d67bf63005d7e07ea59644498da6952
Valentine Dana <valentine.dana@polytechnique.edu>
2023-12-19 15:38:58 +0100

    Adding numpy question file with responses

--- numpy_questions.py
+++ numpy_questions.py
@@ -15,6 +15,8 @@ We also ask to respect the pep8 convention: https://pep8.org.
 This will be enforced with `flake8`. You can check that there is no flake8
 errors by calling `flake8` at the root of the repo.
 """
+
+
 import numpy as np
 
 
@@ -29,7 +31,7 @@ def max_index(X):
     Returns
     -------
     (i, j) : tuple(int)
-        The row and columnd index of the maximum.
+        The row and column index of the maximum.
 
     Raises
     ------
@@ -37,31 +39,38 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-    i = 0
-    j = 0
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        raise ValueError("Input must be a 2D numpy array")
 
-    # TODO
-
+    index = np.argmax(X)
+    i, j = np.unravel_index(index, X.shape)
     return i, j
 
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
 
-    See:
-    https://en.wikipedia.org/wiki/Wallis_product
-
     Parameters
     ----------
     n_terms : int
-        Number of steps in the Wallis product. Note that `n_terms=0` will
-        consider the product to be `1`.
+        Number of steps in the Wallis product.
 
     Returns
     -------
     pi : float
-        The approximation of order `n_terms` of pi using the Wallis product.
+        The approximation of pi using the Wallis product.
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    product = 1.0
+    for i in range(1, n_terms + 1):
+        product *= (4 * i ** 2) / ((4 * i ** 2) - 1)
+    return product * 2
+
+
+# Example usage
+if __name__ == "__main__":
+    # Example for max_index
+    X = np.array([[1, 2, 3], [4, 5, 6]])
+    print("Max index:", max_index(X))
+
+    # Example for wallis_product
+    print("Approximation of pi:", wallis_product(10000))
