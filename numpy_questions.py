e6fb0e254e99a65b2cf7b0e28c6e770bc879b7d5
Zixu JIN <kinsky.jin@gmail.com>
2023-12-20 13:29:12 +0100

    "answers"

--- numpy_questions.py
+++ numpy_questions.py
@@ -37,12 +37,11 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-    i = 0
-    j = 0
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        raise ValueError("Input must be a 2D numpy array")
 
-    # TODO
-
-    return i, j
+    max_element_index = np.unravel_index(np.argmax(X, axis=None), X.shape)
+    return max_element_index
 
 
 def wallis_product(n_terms):
@@ -62,6 +61,11 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    if not isinstance(n_terms, int) or n_terms < 0:
+        raise ValueError("n_terms must be a non-negative integer")
+
+    pi_approx = 1.0
+    for i in range(1, n_terms + 1):
+        pi_approx *= (4 * i ** 2) / ((4 * i ** 2) - 1)
+
+    return pi_approx * 2
