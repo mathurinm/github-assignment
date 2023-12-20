f41f769c48405eb65f93e75e6f5d033161783ae3
“hvignial” <hvignial@gmail.com>
2023-12-20 23:40:30 +0100

    Add script

--- numpy_questions.py
+++ numpy_questions.py
@@ -25,43 +25,40 @@ def max_index(X):
     ----------
     X : ndarray of shape (n_samples, n_features)
         The input array.
+
     Returns
     -------
     (i, j) : tuple(int)
-        The row and columnd index of the maximum.
+        The row and column index of the maximum.
+
     Raises
     ------
     ValueError
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-    if not isinstance(X, np.ndarray):
-        raise ValueError("Input must be a numpy array.")
-    if X.ndim != 2:
-        raise ValueError("Input array must be 2D.")
-    max_index = np.argmax(X)
-    max_index_2d = np.unravel_index(max_index, X.shape)
-    return max_index_2d[0], max_index_2d[1]
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        raise ValueError("Input must be a 2D numpy array")
+
+    index = np.argmax(X)
+    i, j = np.unravel_index(index, X.shape)
+    return i, j
 
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
 
-    See:
-    https://en.wikipedia.org/wiki/Wallis_product
     Parameters
     ----------
     n_terms : int
-        Number of steps in the Wallis product. Note that ⁠ n_terms=0 ⁠ will
-        consider the product to be ⁠ 1 ⁠.
+        Number of steps in the Wallis product.
+
     Returns
     -------
     pi : float
-        The approximation of order ⁠ n_terms ⁠ of pi using the Wallis product.
+        The approximation of pi using the Wallis product.
     """
-    if n_terms == 0:
-        return 2.0
     product = 1.0
     for i in range(1, n_terms + 1):
-        product = (4 * i * 2) / (4 * i ** 2 - 1)
-    return product
+        product *= (4 * i ** 2) / ((4 * i ** 2) - 1)
+    return product * 2
