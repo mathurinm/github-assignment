fa2a5033d66f2b3e0c0fa8c8de2bd0086551d7d4
Thibaud001 <thibaud.drion@hec.edu>
2024-11-08 16:59:00 +0100

    Thibaud_Drion

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -28,47 +28,48 @@ from sklearn.utils.validation import check_is_fitted
 from sklearn.utils.multiclass import check_classification_targets
 
 
-class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    "OneNearestNeighbor classifier."
+import numpy as np
+from sklearn.base import BaseEstimator
+from sklearn.base import ClassifierMixin
+from sklearn.utils.validation import check_X_y
+from sklearn.utils.validation import check_array
+from sklearn.utils.validation import check_is_fitted
+from sklearn.utils.multiclass import check_classification_targets
 
-    def __init__(self):  # noqa: D107
+class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
+    """OneNearestNeighbor classifier.
+
+    The OneNearestNeighbor classifier predicts for a point X_i the target y_k
+    of the training sample X_k which is the closest to X_i. We measure proximity
+    with the Euclidean distance.
+    """
+
+    def __init__(self):
         pass
 
     def fit(self, X, y):
-        """Write docstring.
 
-        And describe parameters
-        """
         X, y = check_X_y(X, y)
         check_classification_targets(y)
         self.classes_ = np.unique(y)
         self.n_features_in_ = X.shape[1]
-
-        # XXX fix
+        self.X_ = X
+        self.y_ = y
         return self
 
     def predict(self, X):
-        """Write docstring.
 
-        And describe parameters
-        """
         check_is_fitted(self)
         X = check_array(X)
-        y_pred = np.full(
-            shape=len(X), fill_value=self.classes_[0],
-            dtype=self.classes_.dtype
-        )
-
-        # XXX fix
+        y_pred = np.empty(len(X), dtype=self.classes_.dtype)
+        for i, x in enumerate(X):
+            distances = np.linalg.norm(self.X_ - x, axis=1)
+            nearest_neighbor_index = np.argmin(distances)
+            y_pred[i] = self.y_[nearest_neighbor_index]
         return y_pred
 
     def score(self, X, y):
-        """Write docstring.
 
-        And describe parameters
-        """
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
-
-        # XXX fix
-        return y_pred.sum()
+        return np.mean(y_pred == y)
