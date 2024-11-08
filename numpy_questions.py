89661c27f48403607e8cdaa499f91bf9bc2ddb4b
Sijelmassi iliass <iliass.sij@icloud.com>
2024-11-08 15:56:10 +0100

    Numpy_question final commit

--- numpy_questions.py
+++ numpy_questions.py
@@ -1,4 +1,22 @@
+"""Assignment - using numpy and making a PR.
+
+The goals of this assignment are:
+    * Use numpy in practice with two easy exercises.
+    * Use automated tools to validate the code (`pytest` and `flake8`)
+    * Submit a Pull-Request on github to practice `git`.
+
+The two functions below are skeleton functions. The docstrings explain what
+are the inputs, the outputs and the expected error. Fill the function to
+complete the assignment. The code should be able to pass the test that we
+wrote. To run the tests, use `pytest test_numpy_question.py` at the root of
+the repo. It should say that 2 tests ran with success.
+
+We also ask to respect the pep8 convention: https://pep8.org.
+This will be enforced with `flake8`. You can check that there is no flake8
+errors by calling `flake8` at the root of the repo.
+"""
 import numpy as np
+
 
 def max_index(X):
     """Return the index of the maximum in a numpy array.
