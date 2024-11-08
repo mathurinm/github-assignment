54c7d7e48b6ed4477afbe99166b7bc268064517e
Ibrahim Barry <ibrahimbarry2020@gmail.com>
2024-11-08 20:08:30 +0100

    skearn_questions format corrected to pep8

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -6,7 +6,7 @@ estimator for the OneNearestNeighbor and check that it is working properly.
 The nearest neighbor classifier predicts for a point X_i the target y_k of
 the training sample X_k which is the closest to X_i. We measure proximity with
 the Euclidean distance. The model will be evaluated with the accuracy (average
-number of samples corectly classified). You need to implement the `fit`,
+number of samples correctly classified). You need to implement the `fit`,
 `predict` and `score` methods for this class. The code you write should pass
 the test we implemented. You can run the tests by calling at the root of the
 repo `pytest test_sklearn_questions.py`.
@@ -20,11 +20,8 @@ for the methods you code and for the class. The docstring will be checked using
 `pydocstyle` that you can also call at the root of the repo.
 """
 import numpy as np
-from sklearn.base import BaseEstimator
-from sklearn.base import ClassifierMixin
-from sklearn.utils.validation import check_X_y
-from sklearn.utils.validation import check_array
-from sklearn.utils.validation import check_is_fitted
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.multiclass import check_classification_targets
 
 
@@ -43,6 +40,7 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
     n_features_in_ : int
         The number of features in the input data.
     """
+
     def __init__(self):  # noqa: D107
         pass
 
