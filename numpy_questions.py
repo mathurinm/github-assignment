0bdfc1225650ff8185c100b331411bcc2f673ad7
Augustin Naton <augustin.naton@gmail.com>
2024-11-08 16:56:07 +0100

    Corrected flake8 lint

--- numpy_questions.py
+++ numpy_questions.py
@@ -37,7 +37,7 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-    if type(X) != np.ndarray:
+    if type(X) is not np.ndarray:
         raise ValueError
 
     if len(X.shape) != 2:
