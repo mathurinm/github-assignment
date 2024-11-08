c9b37d76597ac5c5faeb1faf20691fb914d9e587
Sheetal-12 <sheetal.popat@somaiya.edu>
2024-11-08 17:07:29 +0100

    fixed errors

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -29,7 +29,7 @@ from sklearn.utils.multiclass import check_classification_targets
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    "OneNearestNeighbor classifier."
+    """OneNearestNeighbor classifier."""
 
     def __init__(self):  # noqa: D107
         pass
