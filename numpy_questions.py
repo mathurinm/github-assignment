87cfa507a54d2f1964f0eca015423e1f2f1fa5ba
Sijelmassi iliass <iliass.sij@icloud.com>
2024-11-08 15:51:26 +0100

    Numpy_questions 2

--- numpy_questions.py
+++ numpy_questions.py
@@ -1,22 +1,4 @@
-"""Assignment - using numpy and making a PR.
-
-The goals of this assignment are:
-    * Use numpy in practice with two easy exercises.
-    * Use automated tools to validate the code (`pytest` and `flake8`)
-    * Submit a Pull-Request on github to practice `git`.
-
-The two functions below are skeleton functions. The docstrings explain what
-are the inputs, the outputs and the expected error. Fill the function to
-complete the assignment. The code should be able to pass the test that we
-wrote. To run the tests, use `pytest test_numpy_question.py` at the root of
-the repo. It should say that 2 tests ran with success.
-
-We also ask to respect the pep8 convention: https://pep8.org.
-This will be enforced with `flake8`. You can check that there is no flake8
-errors by calling `flake8` at the root of the repo.
-"""
 import numpy as np
-
 
 def max_index(X):
     """Return the index of the maximum in a numpy array.
@@ -29,7 +11,7 @@ def max_index(X):
     Returns
     -------
     (i, j) : tuple(int)
-        The row and columnd index of the maximum.
+        The row and column index of the maximum.
 
     Raises
     ------
@@ -37,51 +19,43 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-    # Check if X is a numpy array and if it's 2D
     if not isinstance(X, np.ndarray):
         raise ValueError("Input must be a numpy array.")
     if X.ndim != 2:
         raise ValueError("Input array must be 2D.")
 
-    # Get the index of the maximum element by iterating
     max_value = X[0, 0]
     max_position = (0, 0)
-    
+
     for i in range(X.shape[0]):
         for j in range(X.shape[1]):
             if X[i, j] > max_value:
                 max_value = X[i, j]
                 max_position = (i, j)
-                
+
     return max_position
 
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
 
-    See:
-    https://en.wikipedia.org/wiki/Wallis_product
-
     Parameters
     ----------
     n_terms : int
-        Number of steps in the Wallis product. Note that `n_terms=0` will
-        consider the product to be `1`.
+        Number of steps in the Wallis product.
 
     Returns
     -------
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    
-    if n_terms == 0 :
+    if n_terms == 0:
         return 1.0
-    
-    p = 1 
-    for k in range(1, n_terms+1) : 
-        num = 4* k**2
+
+    p = 1
+    for k in range(1, n_terms + 1):
+        num = 4 * k**2
         den = num - 1
-        p *= (num/den)
+        p *= (num / den)
+
     return 2 * p
