79c9a75b3044c2ef63186c84ac4baa2eb01d0ae0
Adrien Senghor <adriensenghor@MacBook-Air-de-Adrien.local>
2024-11-08 16:14:41 +0100

    adriensenghor numpy good

--- numpy_questions.py
+++ numpy_questions.py
@@ -8,7 +8,7 @@ The goals of this assignment are:
 The two functions below are skeleton functions. The docstrings explain what
 are the inputs, the outputs and the expected error. Fill the function to
 complete the assignment. The code should be able to pass the test that we
-wrote. To run the tests, use `pytest test_numpy_question.py` at the root of
+wrote. To run the tests, use `pytest test_numpy_questions.py` at the root of
 the repo. It should say that 2 tests ran with success.
 
 We also ask to respect the pep8 convention: https://pep8.org.
@@ -40,13 +40,13 @@ def max_index(X):
     i = 0
     j = 0
 
-    if len(X.shape) != 2:
-        return print('the value is not a Numpy array')
-    
-    elif not isinstance(X, np.ndarray):
-        return print('the shape is not 2D')
-    
-    else:
+    if not isinstance(X, np.ndarray):
+        raise ValueError('the shape is not 2D')
+
+    elif len(X.shape) != 2:
+        raise ValueError('the value is not a Numpy array')
+
+    elif len(X.shape) == 2:
         nb_max = np.where(X == X.max())
         i = nb_max[0][0]
         j = nb_max[1][0]
@@ -72,15 +72,10 @@ def wallis_product(n_terms):
     """
     if n_terms == 0:
         return 1
-    
+
     else:
         A = np.arange(1, n_terms+1)
         ones = np.ones(n_terms)
         wallis = np.prod((4*(A**2))/((4*(A**2))-ones))
         result = 2*wallis
         return result
-
-
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-
