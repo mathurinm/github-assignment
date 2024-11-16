8d9fb35842975b73f52f1cf43bed90c04803f528
Lea Tinelli <leatinelli@air-de-lea.lan>
2024-11-16 15:36:57 +0100

    Numpy Questions

--- numpy_questions.py
+++ numpy_questions.py
@@ -15,6 +15,7 @@ We also ask to respect the pep8 convention: https://pep8.org.
 This will be enforced with `flake8`. You can check that there is no flake8
 errors by calling `flake8` at the root of the repo.
 """
+
 import numpy as np
 
 
@@ -40,7 +41,13 @@ def max_index(X):
     i = 0
     j = 0
 
-    # TODO
+    if not isinstance(X, np.ndarray):
+        raise ValueError("The input must be a Numpy Array")
+
+    if X.ndim != 2:
+        raise ValueError("The array must be in 2D")
+
+    i, j = np.unravel_index(np.argmax(X, axis=None), X.shape)
 
     return i, j
 
@@ -62,6 +69,12 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    if n_terms == 0:
+        return 1.0
+
+    product = 1.0
+    for n in range(1, n_terms + 1):
+        term = (4 * n**2) / ((4 * n**2) - 1)
+        product *= term
+
+    return 2 * product
