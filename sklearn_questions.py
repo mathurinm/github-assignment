21c1dd50ae5b929d6b18fcf2db3e86d65b4aa44e
Remogie <rems.laget@gmail.com>
2024-11-16 15:48:45 +0100

    will it finally work

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -1,31 +1,99 @@
-# ##################################################
-# YOU SHOULD NOT TOUCH THIS FILE !
-# ##################################################
+"""Assignment - making a sklearn estimator.
+The goal of this assignment is to implement by yourself a scikit-learn
+estimator for the OneNearestNeighbor and check that it is working properly.
+The nearest neighbor classifier predicts for a point X_i the target y_k of
+the training sample X_k which is the closest to X_i. We measure proximity with
+the Euclidean distance. The model will be evaluated with the accuracy (average
+number of samples corectly classified). You need to implement the `fit`,
+`predict` and `score` methods for this class. The code you write should pass
+the test we implemented. You can run the tests by calling at the root of the
+repo `pytest test_sklearn_questions.py`.
+We also ask to respect the pep8 convention: https://pep8.org. This will be
+enforced with `flake8`. You can check that there is no flake8 errors by
+calling `flake8` at the root of the repo.
+Finally, you need to write docstring similar to the one in `numpy_questions`
+for the methods you code and for the class. The docstring will be checked using
+`pydocstyle` that you can also call at the root of the repo.
+"""
+import numpy as np
+from sklearn.base import BaseEstimator
+from sklearn.base import ClassifierMixin
+from sklearn.utils.validation import check_X_y
+from sklearn.utils.validation import check_array
+from sklearn.utils.validation import check_is_fitted
+from sklearn.utils.multiclass import check_classification_targets
 
-from sklearn.utils.estimator_checks import check_estimator
-from sklearn.model_selection import train_test_split
-from sklearn.datasets import make_classification
-from sklearn.neighbors import KNeighborsClassifier
 
-from sklearn_questions import OneNearestNeighbor
+class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
+    """OneNearestNeighbor classifier."""
 
-from numpy.testing import assert_array_equal
+    def __init__(self):  # noqa: D107
+        pass
 
+    def fit(self, X, y):
+        """Fit the OneNearestNeighbor model on training data.
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Training data.
+        y : array-like of shape (n_samples,)
+            Target values.
+        Returns
+        -------
+        self : object
+            Fitted estimator.
+        """
+        X, y = check_X_y(X, y)
+        check_classification_targets(y)
+        self.classes_ = np.unique(y)
+        self.n_features_in_ = X.shape[1]
 
-def test_one_nearest_neighbor_check_estimator():
-    check_estimator(OneNearestNeighbor())
+        self.X_train_ = X
+        self.y_train_ = y
 
+        return self
 
-def test_one_nearest_neighbor_match_sklearn():
-    X, y = make_classification(n_samples=200, n_features=20,
-                               random_state=42)
-    X_train, X_test, y_train, y_test = \
-        train_test_split(X, y, random_state=42)
-    knn = KNeighborsClassifier(n_neighbors=1)
-    y_pred_sk = knn.fit(X_train, y_train).predict(X_test)
+    def predict(self, X):
+        """Predict the class labels for the provided data.
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Test samples.
+        Returns
+        -------
+        y_pred : ndarray of shape (n_samples,)
+            Predicted class labels for each data sample.
+        """
+        check_is_fitted(self)
+        X = check_array(X)
 
-    onn = OneNearestNeighbor()
-    y_pred_me = onn.fit(X_train, y_train).predict(X_test)
-    assert_array_equal(y_pred_me, y_pred_sk)
+        y_pred = np.empty(len(X), dtype=self.y_train_.dtype)
 
-    assert onn.score(X_test, y_test) == knn.score(X_test, y_test)
+        for i, x in enumerate(X):
+            distances = np.sqrt(((self.X_train_ - x) ** 2).sum(axis=1))
+            nearest_neighbor_idx = np.argmin(distances)
+            y_pred[i] = self.y_train_[nearest_neighbor_idx]
+
+        return y_pred
+
+    def score(self, X, y):
+        """Return the accuracy of the model.
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Test samples.
+        y : array-like of shape (n_samples,)
+            True labels for `X`.
+        And describe parameters
+        Returns
+        -------
+        accuracy : float
+            Accuracy of the model,
+            the proportion of correctly classified samples.
+        """
+        X, y = check_X_y(X, y)
+        y_pred = self.predict(X)
+
+        # Calculate accuracy
+        accuracy = np.mean(y_pred == y)
+        return accuracy
