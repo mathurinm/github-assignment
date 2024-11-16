7aeaab13672608b2f43f014e670a0bc8c5844dd5
Remogie <rems.laget@gmail.com>
2024-11-16 15:25:19 +0100

    what about now

--- numpy_questions.py
+++ numpy_questions.py
@@ -1,36 +1,29 @@
 """Assignment - using numpy and making a PR.
-
 The goals of this assignment are:
     * Use numpy in practice with two easy exercises.
     * Use automated tools to validate the code (`pytest` and `flake8`)
     * Submit a Pull-Request on github to practice `git`.
-
 The two functions below are skeleton functions. The docstrings explain what
 are the inputs, the outputs and the expected error. Fill the function to
 complete the assignment. The code should be able to pass the test that we
-wrote. To run the tests, use `pytest test_numpy_questions.py` at the root of
+wrote. To run the tests, use `pytest test_numpy_question.py` at the root of
 the repo. It should say that 2 tests ran with success.
-
 We also ask to respect the pep8 convention: https://pep8.org.
 This will be enforced with `flake8`. You can check that there is no flake8
 errors by calling `flake8` at the root of the repo.
 """
+
 import numpy as np
-
-
 def max_index(X):
     """Return the index of the maximum in a numpy array.
-
     Parameters
     ----------
     X : ndarray of shape (n_samples, n_features)
         The input array.
-
     Returns
     -------
     (i, j) : tuple(int)
         The row and columnd index of the maximum.
-
     Raises
     ------
     ValueError
@@ -40,35 +33,17 @@ def max_index(X):
     i = 0
     j = 0
 
-    # Check if X is a numpy array
     if not isinstance(X, np.ndarray):
         raise ValueError("Input must be a numpy array.")
-
-    # Check if X is 2D
     if X.ndim != 2:
-        raise ValueError("Input array must be 2D.")
-    
-    
-    # Convert the flattened index to 2D (row, column)
+        raise ValueError("Input array must be 2-dimensional.")
+
     i, j = np.unravel_index(np.argmax(X), X.shape)
 
     return i, j
 
-
 def wallis_product(n_terms):
-    """Implement the Wallis product to compute an approximation of pi.
-
-    See:
-    https://en.wikipedia.org/wiki/Wallis_product
-
-    Parameters
-    ----------
-    n_terms : int
-        Number of steps in the Wallis product. Note that `n_terms=0` will
-        consider the product to be `1`.
-
-    Returns
-    -------
+    """""
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
@@ -77,5 +52,6 @@ def wallis_product(n_terms):
 
     product = 1.0
     for n in range(1, n_terms + 1):
-        product *= (4 * n**2) / (4 * n**2 - 1)
-    return product*2
+        term = (4 * n**2) / ((4 * n**2) - 1)
+        product *= term
+    return product * 2
--- sklearn_questions.py
+++ sklearn_questions.py
@@ -1,8 +1,6 @@
 """Assignment - making a sklearn estimator.
-
 The goal of this assignment is to implement by yourself a scikit-learn
 estimator for the OneNearestNeighbor and check that it is working properly.
-
 The nearest neighbor classifier predicts for a point X_i the target y_k of
 the training sample X_k which is the closest to X_i. We measure proximity with
 the Euclidean distance. The model will be evaluated with the accuracy (average
@@ -10,11 +8,9 @@ number of samples corectly classified). You need to implement the `fit`,
 `predict` and `score` methods for this class. The code you write should pass
 the test we implemented. You can run the tests by calling at the root of the
 repo `pytest test_sklearn_questions.py`.
-
 We also ask to respect the pep8 convention: https://pep8.org. This will be
 enforced with `flake8`. You can check that there is no flake8 errors by
 calling `flake8` at the root of the repo.
-
 Finally, you need to write docstring similar to the one in `numpy_questions`
 for the methods you code and for the class. The docstring will be checked using
 `pydocstyle` that you can also call at the root of the repo.
@@ -29,15 +25,23 @@ from sklearn.utils.multiclass import check_classification_targets
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    "OneNearestNeighbor classifier."
+    """OneNearestNeighbor classifier."""
 
     def __init__(self):  # noqa: D107
         pass
 
     def fit(self, X, y):
-        """Write docstring.
-
-        And describe parameters
+        """Fit the OneNearestNeighbor model on training data.
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Training data.
+        y : array-like of shape (n_samples,)
+            Target values.
+        Returns
+        -------
+        self : object
+            Fitted estimator.
         """
         X, y = check_X_y(X, y)
         check_classification_targets(y)
@@ -50,9 +54,15 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         return self
 
     def predict(self, X):
-        """Write docstring.
-
-        And describe parameters
+        """Predict the class labels for the provided data.
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Test samples.
+        Returns
+        -------
+        y_pred : ndarray of shape (n_samples,)
+            Predicted class labels for each data sample.
         """
         check_is_fitted(self)
         X = check_array(X)
@@ -67,11 +77,22 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         return y_pred
 
     def score(self, X, y):
-        """Write docstring.
-
+        """Return the accuracy of the model.
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Test samples.
+        y : array-like of shape (n_samples,)
+            True labels for `X`.
         And describe parameters
+        Returns
+        -------
+        accuracy : float
+            Accuracy of the model,
+            the proportion of correctly classified samples.
         """
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
 
+        # Calculate accuracy
         return np.mean(y_pred == y)
