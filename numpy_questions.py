Fix max_index dimension check and wallis_product formula - all tests pass

numpy_questions.py
@@ -37,12 +37,21 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-    i = 0
-    j = 0
 
-    # TODO
+    if not isinstance(X, np.ndarray):
+        raise ValueError('The input is not an array.')
 
-    return i, j
+    if X.ndim != 2:
+        raise ValueError('The input is not 2D array')
+    
+    overall_max = np.max(X)
+
+    for i in range(np.shape(X)[0]):
+        for j in range(np.shape(X)[1]):
+            if X[i][j] == overall_max:
+                return i, j
+
+    return 0, 0
 
 
 def wallis_product(n_terms):
@@ -62,6 +71,12 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    if n_terms == 0:
+        return 1
+    
+    else:
+        prod = 1
+        for i in range(1, n_terms + 1):
+            prod *= (4 * i ** 2) / (4 * i**2 - 1)
+
+        return prod * 2