63d72736489366480aa6ecc176b78aab3a1d3069
Gaspard Baumelou <gaspard.baumelou8@etu.univ-lorraine.fr>
2025-11-13 22:31:58 +0100

    numpy questions adjustments

--- numpy_questions.py
+++ numpy_questions.py
@@ -72,5 +72,7 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
+    if n_terms == 0:
+        return 1
     res = np.prod(np.array([4*n**2/(4*n**2 - 1) for n in range(1, n_terms+1)]))
     return 2 * res
