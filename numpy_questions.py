c7d2b92062113f45f8812f7685ce87951db22025
NelsonTEJO <lucnelsontejodonkeng@gmail.com>
2025-11-13 18:25:28 +0100

    nouvelle modification

--- numpy_questions.py
+++ numpy_questions.py
@@ -37,21 +37,20 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-    i = 0
-    j = 0
-
-    # TODO
-    if not isinstance(X, np.ndarray) or X.ndim!=2:
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
         raise ValueError("Input array should be a 2-dimensional numpy array")
-    i,j=0,0
-    max=X[0][0]
-    for line in range(X.shape[0]):
-        for c in range(X.shape[1]):
-            if X[line][c]>max:
-                i,j=line,c
-                max=X[line][c]
-            continue
+
+    i, j = 0, 0
+    max_val = X[0][0]
+    for row in range(X.shape[0]):
+        for col in range(X.shape[1]):
+            if X[row][col] > max_val:
+                i, j = row, col
+                max_val = X[row][col]
+
     return i, j
+
+
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
 
@@ -69,13 +68,11 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    res=1
-    if n_terms==0:
+    res = 1
+    if n_terms == 0:
         return 1
-    for k in range(1,n_terms+1):
-        res*=(4*k**2)/(4*k**2 - 1)
-    return 2*res
 
+    for k in range(1, n_terms + 1):
+        res *= (4 * k**2) / (4 * k**2 - 1)
 
+    return 2 * res
