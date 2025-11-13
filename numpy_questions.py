af0b51b6513b3db369906e97e01c67ecdd49f4d2
Nikolai <nikolailevin00@gmail.com>
2025-11-13 23:47:05 +0100

    Fixed pytest and flake8 errors

--- numpy_questions.py
+++ numpy_questions.py
@@ -15,6 +15,7 @@ We also ask to respect the pep8 convention: https://pep8.org.
 This will be enforced with `flake8`. You can check that there is no flake8
 errors by calling `flake8` at the root of the repo.
 """
+
 import numpy as np
 
 
@@ -37,12 +38,18 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-    i = 0
-    j = 0
 
-    # TODO
+    if not isinstance(X, np.ndarray):
+        raise ValueError("Input is not a numpy array")
 
-    return i, j
+    if X.ndim != 2:
+        raise ValueError("Input must be 2D")
+
+    idx_1d = np.argmax(X)
+
+    (i, j) = np.unravel_index(idx_1d, X.shape)
+
+    return (i, j)
 
 
 def wallis_product(n_terms):
@@ -62,6 +69,19 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
+
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+
+    result = 1
+
+    if n_terms == 0:
+        pass
+
+    else:
+        for n in range(1, n_terms + 1):
+            result *= 4 * n**2 / (4 * n**2 - 1)
+
+        result *= 2
+
+    return result
--- sklearn_questions.py
+++ sklearn_questions.py
@@ -19,6 +19,7 @@ Finally, you need to write docstring similar to the one in `numpy_questions`
 for the methods you code and for the class. The docstring will be checked using
 `pydocstyle` that you can also call at the root of the repo.
 """
+
 import numpy as np
 from sklearn.base import BaseEstimator
 from sklearn.base import ClassifierMixin
@@ -28,47 +29,89 @@ from sklearn.utils.validation import check_is_fitted
 from sklearn.utils.multiclass import check_classification_targets
 
 
-class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    "OneNearestNeighbor classifier."
+class OneNearestNeighbor(ClassifierMixin, BaseEstimator):
+    """
+    OneNearestNeighbor Classifier.
+    Estimator that implements the 1-Nearest Neighbor algorithm to predict a new
+    sample's label based on the closest training sample.
 
-    def __init__(self):  # noqa: D107
+    Parameters
+    ----------
+     None
+    """
+
+    def __init__(self):
+        """
+        Init function.
+
+        Returns
+        -------
+        None.
+
+        """
         pass
 
     def fit(self, X, y):
-        """Write docstring.
+        """
+        Parameters
+        ----------
+        X: array of shape (n_samples, n_features)
+        y: array of shape (n_samples,), holds the labels to predict
 
-        And describe parameters
+        Returns
+        -------
+        Self: itself
         """
         X, y = check_X_y(X, y)
         check_classification_targets(y)
-        self.classes_ = np.unique(y)
-        self.n_features_in_ = X.shape[1]
 
-        # XXX fix
+        self.n_features_in_ = X.shape[1]
+        self.X_ = X
+        self.y_ = y
+        self.classes_ = np.unique(y)
+
         return self
 
     def predict(self, X):
-        """Write docstring.
+        """
+        Parameters
+        ----------
+        X: array of shape (n_samples, n_features)
 
-        And describe parameters
+        Returns
+        -------
+        y_pred: the predicted labels for each sample in X
         """
         check_is_fitted(self)
         X = check_array(X)
-        y_pred = np.full(
-            shape=len(X), fill_value=self.classes_[0],
-            dtype=self.classes_.dtype
-        )
 
-        # XXX fix
+        if X.shape[1] != self.n_features_in_:
+            raise ValueError(
+                f"X has {X.shape[1]} features, but OneNearestNeighbor "
+                f"is expecting {self.n_features_in_} features as input."
+            )
+
+        difference = X[:, None, :] - self.X_[None, :, :]
+        distances = np.linalg.norm(difference, axis=2)
+
+        closest_indx = np.argmin(distances, axis=1)
+        y_pred = self.y_[closest_indx]
+
         return y_pred
 
     def score(self, X, y):
-        """Write docstring.
+        """
+        Parameters
+        ----------
+        X: array of shape (n_samples, n_features)
+        y: array of shape (n_samples,), holds the labels to predict
 
-        And describe parameters
+        Returns
+        -------
+        score: float,
+        the mean accuracy of the prediction against the true labels y
         """
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
 
-        # XXX fix
-        return y_pred.sum()
+        return np.mean(y_pred == y)
