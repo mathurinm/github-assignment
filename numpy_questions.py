906f92dca449dccef4a5a36b0a12bc8e1beadb1f
oli <olivier.smith@live.fr>
2025-11-13 15:13:57 +0100

    both exercised solved

--- numpy_questions.py
+++ numpy_questions.py
@@ -37,12 +37,25 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
+    if not isinstance(X, np.ndarray):
+        raise ValueError("Input must be a numpy array")
+
+    if X.ndim != 2:
+        raise ValueError("Input must be a 2D array, but got shape: {}"
+                         .format(X.shape))
+
     i = 0
     j = 0
+    num_rows, num_cols = X.shape
 
-    # TODO
+    max = 0
+    for i in range(num_cols):
+        for j in range(num_rows):
+            if X[i][j] > max:
+                max = X[i][j]
+                res = (i, j)
 
-    return i, j
+    return res
 
 
 def wallis_product(n_terms):
@@ -64,4 +77,11 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+
+    if n_terms == 0:
+        return 1
+    res = 1
+    for i in range(1, n_terms+1):
+        res *= (4 * i**2) / (4 * i**2 - 1)
+
+    return res * 2
