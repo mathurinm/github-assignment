60c14c934ffc403f97c6e1e6d3ac1c3d50dd4f53
刘广悦 <liuguangyue@liuguangyuedeMacBook-Pro.local>
2025-11-14 08:43:02 +0100

    Implement OneNearestNeighbor and fix numpy_questions

--- numpy_questions.py
+++ numpy_questions.py
@@ -19,49 +19,25 @@ import numpy as np
 
 
 def max_index(X):
-    """Return the index of the maximum in a numpy array.
-
-    Parameters
-    ----------
-    X : ndarray of shape (n_samples, n_features)
-        The input array.
-
-    Returns
-    -------
-    (i, j) : tuple(int)
-        The row and columnd index of the maximum.
-
-    Raises
-    ------
-    ValueError
-        If the input is not a numpy array or
-        if the shape is not 2D.
-    """
-    i = 0
-    j = 0
-
-    # TODO
-
+    if not isinstance(X, np.ndarray):
+        raise ValueError("Input must be a numpy array")
+    if X.ndim != 2:
+        raise ValueError("Input must be a 2D array")
+    
+    max_idx = np.argmax(X)
+    i, j = np.unravel_index(max_idx, X.shape)
+    
     return i, j
 
 
+
+
+
 def wallis_product(n_terms):
-    """Implement the Wallis product to compute an approximation of pi.
+    if n_terms == 0:
+        return 1.0
 
-    See:
-    https://en.wikipedia.org/wiki/Wallis_product
-
-    Parameters
-    ----------
-    n_terms : int
-        Number of steps in the Wallis product. Note that `n_terms=0` will
-        consider the product to be `1`.
-
-    Returns
-    -------
-    pi : float
-        The approximation of order `n_terms` of pi using the Wallis product.
-    """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    product = 1.0
+    for k in range(1, n_terms + 1):
+        product *= (4 * k**2) / (4 * k**2 - 1)
+    return 2 * product
