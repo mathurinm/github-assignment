7ea550e7518c146b2e9171218cd190ffb8dff371
刘广悦 <liuguangyue@liuguangyuedeMacBook-Pro.local>
2025-11-14 09:10:23 +0100

    Fix flake8 style issues and remove extra blank lines

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -1,43 +1,15 @@
-"""Assignment - making a sklearn estimator.
-
-Custom implementation of a OneNearestNeighbor classifier.
-"""
-
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
-from sklearn.utils.validation import (
-    check_X_y, check_is_fitted
-)
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.multiclass import check_classification_targets
 
 
-class OneNearestNeighbor(ClassifierMixin, BaseEstimator):
-    """One Nearest Neighbor classifier.
-
-    Assigns to each sample the label of the closest training point
-    using Euclidean distance.
-    """
-
+class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
     def __init__(self):
         pass
 
     def fit(self, X, y):
-        """Fit the OneNearestNeighbor classifier.
-
-        Parameters
-        ----------
-        X : ndarray of shape (n_samples, n_features)
-            Training data.
-
-        y : ndarray of shape (n_samples,)
-            Target labels.
-
-        Returns
-        -------
-        self : object
-            Fitted classifier.
-        """
-        X, y = self._validate_data(X, y, reset=True)
+        X, y = check_X_y(X, y)
         check_classification_targets(y)
 
         self.X_train_ = X
@@ -47,32 +19,16 @@ class OneNearestNeighbor(ClassifierMixin, BaseEstimator):
         return self
 
     def predict(self, X):
-        """Predict class labels for samples in X.
-
-        Parameters
-        ----------
-        X : ndarray of shape (n_samples, n_features)
-            Input samples.
-
-        Returns
-        -------
-        y_pred : ndarray of shape (n_samples,)
-            Predicted labels.
-        """
         check_is_fitted(self)
-
-        X = self._validate_data(X, reset=False)
+        X = check_array(X)
 
         distances = np.linalg.norm(
             X[:, None, :] - self.X_train_[None, :, :],
-            axis=2
-        )
-
+            axis=2)
         nearest_idx = np.argmin(distances, axis=1)
         return self.y_train_[nearest_idx]
 
     def score(self, X, y):
-        """Return accuracy score."""
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
         return np.mean(y_pred == y)
