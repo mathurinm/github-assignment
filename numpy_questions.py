7e4824dde2ba53202ed51623e952756fe55b8de4
leroso <mohamed.soro@ensea.edu.ci>
2025-11-15 09:35:50 +0100

    first commit

--- numpy_questions.py
+++ numpy_questions.py
@@ -15,7 +15,7 @@ We also ask to respect the pep8 convention: https://pep8.org.
 This will be enforced with `flake8`. You can check that there is no flake8
 errors by calling `flake8` at the root of the repo.
 """
-import numpy as np
+#import numpy as np
 
 
 def max_index(X):
