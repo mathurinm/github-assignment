a73702cbc454fe6e281a4e3702a6237eb322e606
Arthur <arthur_gaillard@yahoo.com>
2025-11-15 16:03:51 +0100

    Part B ArthurGaillard

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -1,29 +1,8 @@
-"""Assignment - making a sklearn estimator.
-
-The goal of this assignment is to implement by yourself a scikit-learn
-estimator for the OneNearestNeighbor and check that it is working properly.
-
-The nearest neighbor classifier predicts for a point X_i the target y_k of
-the training sample X_k which is the closest to X_i. We measure proximity with
-the Euclidean distance. The model will be evaluated with the accuracy (average
-number of samples corectly classified). You need to implement the `fit`,
-`predict` and `score` methods for this class. The code you write should pass
-the test we implemented. You can run the tests by calling at the root of the
-repo `pytest test_sklearn_questions.py`.
-
-We also ask to respect the pep8 convention: https://pep8.org. This will be
-enforced with `flake8`. You can check that there is no flake8 errors by
-calling `flake8` at the root of the repo.
-
-Finally, you need to write docstring similar to the one in `numpy_questions`
-for the methods you code and for the class. The docstring will be checked using
-`pydocstyle` that you can also call at the root of the repo.
-"""
+"""Assignment - making a sklearn estimator."""
 
 import numpy as np
 from sklearn.base import ClassifierMixin, BaseEstimator
-from sklearn.utils.validation import validate_data
-from sklearn.utils.validation import check_is_fitted
+from sklearn.utils.validation import check_array, check_is_fitted
 from sklearn.utils.multiclass import check_classification_targets
 
 
@@ -48,18 +27,12 @@ class OneNearestNeighbor(ClassifierMixin, BaseEstimator):
         self : object
             Fitted estimator.
         """
-        X, y = validate_data(
-            self,
-            X,
-            y,
-            ensure_2d=True,
-            dtype="numeric")
-
+        X = check_array(X, ensure_2d=True)
+        y = check_array(y, ensure_2d=False)
         check_classification_targets(y)
 
         self.X_ = X
         self.y_ = y
-
         self.classes_ = np.unique(y)
         self.n_features_in_ = X.shape[1]
 
@@ -71,29 +44,27 @@ class OneNearestNeighbor(ClassifierMixin, BaseEstimator):
         Parameters
         ----------
         X : array-like of shape (n_samples, n_features)
-            The samples for which we want to guess the label. They must have
-            the same number of features as the data used during ``fit``.
+            Samples for which to predict labels.
 
         Returns
         -------
         y_pred : ndarray of shape (n_samples,)
-            The predicted labels. For each sample, the model looks for the
-            closest point in the training set and returns its label.
+            Predicted labels.
         """
         check_is_fitted(self)
-        X = validate_data(
-            self,
-            X,
-            ensure_2d=True,
-            dtype="numeric",
-            reset=False
+        X = check_array(X, ensure_2d=True)
+
+        if X.shape[1] != self.n_features_in_:
+            raise ValueError(
+                f"X has {X.shape[1]} features, but "
+                f"{self.__class__.__name__} is expecting "
+                f"{self.n_features_in_} features as input"
             )
 
         distance = np.linalg.norm(self.X_[None, :, :] - X[:, None, :], axis=2)
         nearest_index = np.argmin(distance, axis=1)
-        y_pred = self.y_[nearest_index]
 
-        return y_pred
+        return self.y_[nearest_index]
 
     def score(self, X, y):
         """Compute the accuracy of the classifier.
@@ -102,21 +73,23 @@ class OneNearestNeighbor(ClassifierMixin, BaseEstimator):
         ----------
         X : array-like of shape (n_samples, n_features)
             Test samples used to evaluate the model.
-
         y : array-like of shape (n_samples,)
-            True labels corresponding to X.
+            True labels.
 
         Returns
         -------
         score : float
             The accuracy of the predictions, between 0 and 1.
         """
-        X, y = validate_data(
-            self,
-            X, y,
-            ensure_2d=True,
-            dtype=None,
-            reset=False
+        check_is_fitted(self)
+        X = check_array(X, ensure_2d=True)
+        y = check_array(y, ensure_2d=False)
+
+        if X.shape[1] != self.n_features_in_:
+            raise ValueError(
+                f"X has {X.shape[1]} features, but "
+                f"{self.__class__.__name__} is expecting "
+                f"{self.n_features_in_} features as input"
             )
         y_pred = self.predict(X)
 
