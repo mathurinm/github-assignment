57fc13da04669a22a07047669b9d3752b73d18b4
Julia Rouzou <juliarouzou@gmail.com>
2025-11-15 10:39:42 +0100

    Completed numpy assignment-Julia Rouzou

--- numpy_questions.py
+++ numpy_questions.py
@@ -40,10 +40,15 @@ def max_index(X):
     i = 0
     j = 0
 
-    # TODO
+    if not isinstance(X, np.ndarray):
+        raise ValueError("Input must be a numpy array.")
 
+    if X.ndim != 2:
+        raise ValueError("Input array must be 2-dimensional.")
+
+    flat_index = np.argmax(X)
+    i, j = np.unravel_index(flat_index, X.shape)
     return i, j
-
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
@@ -62,6 +67,12 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    if n_terms < 0:
+        raise ValueError("n_terms must be non-negative.")
+
+    product = 1.0
+    for n in range(1, n_terms + 1):
+        product *= (4 * n ** 2) / (4 * n ** 2 - 1)
+
+    return 2 * product
+
