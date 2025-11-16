84c554308c7c25add4dff8e6cb9901adce3fd02a
ryanbalech1 <ryan.balech@polytechnique.edu>
2025-11-16 15:46:01 +0100

    Implement OneNearestNeighbor without validate_data

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -2,38 +2,18 @@
 
 The goal of this assignment is to implement by yourself a scikit-learn
 estimator for the OneNearestNeighbor and check that it is working properly.
-
-The nearest neighbor classifier predicts for a point X_i the target y_k of
-the training sample X_k which is the closest to X_i. We measure proximity with
-the Euclidean distance. The model will be evaluated with the accuracy (average
-number of samples corectly classified). You need to implement the `fit`,
-`predict` and `score` methods for this class. The code you write should pass
-the test we implemented. You can run the tests by calling at the root of the
-repo `pytest test_sklearn_questions.py`.
-
-We also ask to respect the pep8 convention: https://pep8.org. This will be
-enforced with `flake8`. You can check that there is no flake8 errors by
-calling `flake8` at the root of the repo.
-
-Finally, you need to write docstring similar to the one in `numpy_questions`
-for the methods you code and for the class. The docstring will be checked using
-`pydocstyle` that you can also call at the root of the repo.
 """
 import numpy as np
-from sklearn.base import ClassifierMixin, BaseEstimator
-from sklearn.utils.validation import check_is_fitted
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.multiclass import check_classification_targets
 
 
 class OneNearestNeighbor(ClassifierMixin, BaseEstimator):
-    """One nearest neighbor classifier.
-
-    This estimator predicts the label of each sample using the label of
-    the closest training sample according to the Euclidean distance.
-    """
+    """One nearest neighbor classifier."""
 
     def __init__(self):  # noqa: D107
-        # No hyper-parameters for this simple estimator.
+        # no hyperparameters
         pass
 
     def fit(self, X, y):
@@ -43,24 +23,22 @@ class OneNearestNeighbor(ClassifierMixin, BaseEstimator):
         ----------
         X : array-like of shape (n_samples, n_features)
             Training input samples.
+
         y : array-like of shape (n_samples,)
-            Target labels.
+            Training class labels.
 
         Returns
         -------
         self : OneNearestNeighbor
-            Fitted estimator.
+            The fitted classifier.
         """
-        # validate_data sets n_features_in_ and does input validation
-        X, y = validate_data(self, X, y)
+        X, y = check_X_y(X, y)
         check_classification_targets(y)
 
-        # Store training data
-        self.X_train_ = X
-        self.y_train_ = y
-
-        # Attributes expected by scikit-learn
         self.classes_ = np.unique(y)
+        self.X_ = X
+        self.y_ = y
+        self.n_features_in_ = X.shape[1]
 
         return self
 
@@ -77,41 +55,45 @@ class OneNearestNeighbor(ClassifierMixin, BaseEstimator):
         y_pred : ndarray of shape (n_samples,)
             Predicted class labels.
         """
-        check_is_fitted(self)
+        check_is_fitted(self, attributes=["X_", "y_"])
+        X = check_array(X)
 
-        # Use validate_data with reset=False so n_features_in_ is checked
-        X = validate_data(self, X, reset=False)
+        # Enforce consistency of number of features with training data
+        if X.shape[1] != self.n_features_in_:
+            raise ValueError(
+                f"X has {X.shape[1]} features, but "
+                f"{self.__class__.__name__} is expecting "
+                f"{self.n_features_in_} features as input"
+            )
 
-        # Compute Euclidean distance from each test point to each train point
+        # Compute Euclidean distances to all training points
         # X shape: (n_test, n_features)
-        # self.X_train_ shape: (n_train, n_features)
-        diff = X[:, np.newaxis, :] - self.X_train_[np.newaxis, :, :]
-        distances = np.linalg.norm(diff, axis=2)  # (n_test, n_train)
-
-        # Index of the nearest neighbor for each test sample
+        # self.X_ shape: (n_train, n_features)
+        distances = np.linalg.norm(
+            self.X_[np.newaxis, :, :] - X[:, np.newaxis, :],
+            axis=2,
+        )
         nearest_idx = np.argmin(distances, axis=1)
-
-        # Predicted label is label of the nearest training point
-        y_pred = self.y_train_[nearest_idx]
+        y_pred = self.y_[nearest_idx]
 
         return y_pred
 
     def score(self, X, y):
-        """Compute the mean accuracy on the given test data and labels.
+        """Return the accuracy of the classifier on the given test data.
 
         Parameters
         ----------
         X : array-like of shape (n_samples, n_features)
             Test samples.
+
         y : array-like of shape (n_samples,)
-            True labels for X.
+            True class labels.
 
         Returns
         -------
         score : float
-            Mean accuracy of predictions on X versus y.
+            Classification accuracy.
         """
-        # Validate and also check n_features_in_ against what was seen in fit
-        X, y = validate_data(self, X, y, reset=False)
+        X, y = check_X_y(X, y)
         y_pred = self.predict(X)
         return float(np.mean(y_pred == y))
