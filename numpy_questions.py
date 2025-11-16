559dccc88025241c14a2e9c85da32cf3971de142
ryanbalech1 <ryan.balech@polytechnique.edu>
2025-11-16 14:46:06 +0100

    Complete numpy assignment (Ryan Balech)

--- numpy_questions.py
+++ numpy_questions.py
@@ -1,67 +1,49 @@
-"""Assignment - using numpy and making a PR.
+# """Assignment - using numpy and making a PR.
 
-The goals of this assignment are:
-    * Use numpy in practice with two easy exercises.
-    * Use automated tools to validate the code (`pytest` and `flake8`)
-    * Submit a Pull-Request on github to practice `git`.
+# The goals of this assignment are:
+#     * Use numpy in practice with two easy exercises.
+#     * Use automated tools to validate the code (`pytest` and `flake8`)
+#     * Submit a Pull-Request on github to practice `git`.
 
-The two functions below are skeleton functions. The docstrings explain what
-are the inputs, the outputs and the expected error. Fill the function to
-complete the assignment. The code should be able to pass the test that we
-wrote. To run the tests, use `pytest test_numpy_questions.py` at the root of
-the repo. It should say that 2 tests ran with success.
+# The two functions below are skeleton functions. The docstrings explain what
+# are the inputs, the outputs and the expected error. Fill the function to
+# complete the assignment. The code should be able to pass the test that we
+# wrote. To run the tests, use `pytest test_numpy_questions.py` at the root of
+# the repo. It should say that 2 tests ran with success.
 
-We also ask to respect the pep8 convention: https://pep8.org.
-This will be enforced with `flake8`. You can check that there is no flake8
-errors by calling `flake8` at the root of the repo.
-"""
+# We also ask to respect the pep8 convention: https://pep8.org.
+# This will be enforced with `flake8`. You can check that there is no flake8
+# errors by calling `flake8` at the root of the repo.
+# """
 import numpy as np
 
 
 def max_index(X):
-    """Return the index of the maximum in a numpy array.
+    if not isinstance(X, np.ndarray):
+        raise ValueError("Input must be a numpy array.")
+    if X.ndim != 2:
+        raise ValueError("Input must be a 2D numpy array.")
 
-    Parameters
-    ----------
-    X : ndarray of shape (n_samples, n_features)
-        The input array.
+    # Flatten index of the max
+    flat_index = np.argmax(X)
 
-    Returns
-    -------
-    (i, j) : tuple(int)
-        The row and columnd index of the maximum.
-
-    Raises
-    ------
-    ValueError
-        If the input is not a numpy array or
-        if the shape is not 2D.
-    """
-    i = 0
-    j = 0
-
-    # TODO
+    # Convert to 2D index
+    i, j = np.unravel_index(flat_index, X.shape)
 
     return i, j
 
 
 def wallis_product(n_terms):
-    """Implement the Wallis product to compute an approximation of pi.
+    if n_terms < 0:
+        raise ValueError("n_terms must be non-negative.")
 
-    See:
-    https://en.wikipedia.org/wiki/Wallis_product
+    if n_terms == 0:
+        return 1.0
 
-    Parameters
-    ----------
-    n_terms : int
-        Number of steps in the Wallis product. Note that `n_terms=0` will
-        consider the product to be `1`.
+    n = np.arange(1, n_terms + 1)
+    terms = (4 * n * n) / (4 * n * n - 1)
 
-    Returns
-    -------
-    pi : float
-        The approximation of order `n_terms` of pi using the Wallis product.
-    """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    product = np.prod(terms)
+
+    return 2 * product
+
